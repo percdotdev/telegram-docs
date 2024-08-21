69cd026d379383d9af194a71b30ec217d5eda36b
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-08-21 06:50:35 +0200

    chore: Update landing page title with animated text effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Link from 'next/link';
+import { TextEffect } from '@components/text-effect';
 
 export default function HomePage() {
   return (
     <main className="flex h-screen flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      {/* <h1 className="mb-4 text-2xl font-bold">Hello World</h1> */}
+      <h1 className="mb-4 text-4xl font-bold">
+        <TextEffect per='char' preset='blur'>
+          Hello World
+        </TextEffect>
+      </h1>
       <p className="text-fd-muted-foreground">
         You can open{' '}
         <Link
